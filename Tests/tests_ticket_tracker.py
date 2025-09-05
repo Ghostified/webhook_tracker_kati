62445e1808132c788351ac4a2de00315496e2fce
Ghostified <bransonallan@gmail.com>
2025-09-06 00:00:29 +0300

    dev

--- Tests/tests_ticket_tracker.py
+++ Tests/tests_ticket_tracker.py
@@ -3,4 +3,15 @@ import os
 import json
 from ticket_tracker import TicketTracker
 
-#Temporary test DB
+#Test Database File 
+TEST_DB = "test_tickets.json"
+
+class TestTicketTracker(unittest.TestCase):
+
+  def setUp(self):
+    """Run before each test"""
+    # Remove test DB if it exists
+    if os.path.exists(TEST_DB):
+      os.remove(TEST_DB)
+    #Create FResh Tracker
+    
