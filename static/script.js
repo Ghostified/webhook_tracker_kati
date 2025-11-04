a47d761e49966abe3145a9d343c1ab521bd0dd40
Ghostified <bransonallan@gmail.com>
2025-11-04 23:06:12 +0300

    dev'

--- static/script.js
+++ static/script.js
@@ -138,11 +138,7 @@ function renderTickets(container, data){
     if (stepFilter && (ticket.step || '').toLowerCase() !== stepFilter) return false;
     if (idFilter && !String(ticket.id || ticket.ticket_id).toLocaleLowerCase().includes(idFilter)) return false;
   })
-    //Apply Filters
-  const filtered = tickets.filter(ticket => {
-    if (stepFilter && (ticket.step || '').toLowerCase() !== stepFilter) return false;
-    if (idFilter && !String(ticket.id || ticket.ticket_id).toLocaleLowerCase().includes(idFilter)) return false;
-  })
+
 
   //combine date and time inputs into fulltime datetime objects
 
